3f4a39701d6668ec67c1e60cfb1ce3204be01bf3
Mohammad Yassen <mohammadgithub9989@gmail.com>
2026-01-11 18:28:43 +0300

    added parent controller

--- controllers/parent.py
+++ controllers/parent.py
@@ -1,21 +1,62 @@
 from controller import Robot
 
-PATH_START = -0.65
-SCC = 0.25 # Space between 2 cubes
-SPP = 0.3   # Space between 2 bases
-SES = 0.45  # White Sapce
-COLORS = ["red", "green", "blue", "yellow"]
+
 SPEED = 5.0
-
-last_cube_x = PATH_START + (3 * SCC)
-
-BASE_POSITIONS = {
-    color: (last_cube_x + SES) + (i * SPP) for i, color in enumerate(COLORS)
-}
 
 class ParentController(Robot):
     def __init__(self):
         super(ParentController, self).__init__()
+        self.timestep = int(self.getBasicTimeStep())
+
+        # Receiver Initialization ------------------------------
+        self.receiver = self.getDevice("receiver")
+        self.receiver.enable(self.timestep)
+        self.receiver.setChannel(1)
+        
+        
+        # GPS Initialization -----------------------------------
+        self.gps = self.getDevice("gps")  # Ensure the name matches the .wbt file
+        if self.gps:
+            self.gps.enable(self.timestep)
+        else:
+            print(
+                "Error: GPS device not found. Add a GPS node to your robot in the scene tree."
+            )
+
+        
+        # Wheels -----------------------------------------------
+        self.wheels = []
+        # Mapping:
+        # fl (Front Left)  -> wheel2
+        # fr (Front Right) -> wheel1
+        # bl (Back Left)   -> wheel4
+        # br (Back Right)  -> wheel3
+        wheel_names = ["wheel2", "wheel1", "wheel4", "wheel3"]
+
+        for name in wheel_names:
+            wheel = self.getDevice(name)
+            if wheel:
+                wheel.setPosition(float("inf"))
+                wheel.setVelocity(0.0)
+                self.wheels.append(wheel)
+            else:
+                print(f"Error: Wheel {name} not found")
+
+        # Check if we found all wheels
+        if len(self.wheels) != 4:
+            print("Error: Could not find all wheels!")
+            
+        # Arm ---------------------------------------------------
+        self.arm_motors = []
+        for i in range(1, 6):
+            motor = self.getDevice("arm" + str(i))
+            self.arm_motors.append(motor)
+
+        # Gripper ------------------------------------------------
+        self.fingers = []
+        for name in ["finger::left", "finger::right"]:
+            gripper = self.getDevice(name)
+            self.fingers.append(gripper)
    
     def get_position(self):
         if self.gps:
